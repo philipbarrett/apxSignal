4acf42eeca81387ce82833c7dc42b61c3c61bb7b
Philip Barrett <pobarrett@gmail.com>
2016-03-14 15:37:05 -0500

    Added exact Gaussian filter.

--- charts.R
+++ charts.R
@@ -51,19 +51,21 @@ v.y <- 1 * ( v.x > theta.hat )
 kappa <- 10
 mu.sig2.0 <- c( 0, 1 )
 thresh.ukf <- ukf.compute( mu.sig2.0[1], mu.sig2.0[2] , v.y, f, g, Q, R, 1, alpha=1, kappa=kappa, quad = F )
-thresh.ukf.mc <- ukf.compute( mu.sig2.0[1], mu.sig2.0[2] , v.y, f, g, Q, R, 1, alpha=1, kappa=kappa, quad = F, n.mc=10000 )
+# thresh.ukf.mc <- ukf.compute( mu.sig2.0[1], mu.sig2.0[2] , v.y, f, g, Q, R, 1, alpha=1, kappa=kappa, quad = F, n.mc=10000 )
 thresh.ukf.quad <- ukf.compute( mu.sig2.0[1], mu.sig2.0[2] , v.y, f, g, Q, R, 1, alpha=1, kappa=kappa, quad = T )
 # The UKF (using various integration rules)
 thresh <- thresh_filter( mu.sig2.0, rep(0,K), sig.eps, rho, v.y )
+thresh.gf <- gauss_filter( mu.sig2.0, rep(0,K), sig.eps, rho, v.y )
 # The threshold filter
 
 #### THIS CHART INCLUDED ####
 plot( c(1,K), range( c(thresh.ukf$m, v.x, thresh[,1]) ), type='n', xlab='Period', 
       ylab='x' )
 points( 1:K, 1.1 * sd(v.x) * ( 2*v.y-1 ), pch=19, col=alpha('darkgreen', .5), cex=.5 )
-lines( 1:K, thresh.ukf$m.pred[-(K+1)], col='red', lwd=2 )
-lines( 1:K, thresh.ukf.mc$m.pred[-(K+1)], col='red', lwd=1, lty=2 )
-lines( 1:K, thresh.ukf.quad$m.pred[-(K+1)], col='red', lwd=1, lty=3 )
+lines( 1:K, thresh.gf[-(K+1),1], col='red', lwd=2 )
+lines( 1:K, thresh.ukf$m.pred[-(K+1)], col='red', lwd=1, lty=2 )
+# lines( 1:K, thresh.ukf.mc$m.pred[-(K+1)], col='red', lwd=1, lty=3 )
+# lines( 1:K, thresh.ukf.quad$m.pred[-(K+1)], col='red', lwd=1, lty=3 )
 # First point is the period 0 predictor for period 1 => Last point predicts
 # period K+1
 lines( 1:K, thresh[-(K+1),1], col='blue', lwd=2 )
@@ -71,41 +73,51 @@ lines( 1:K, thresh[-(K+1),1], col='blue', lwd=2 )
 # lines( 1:K, thresh.ukf$m + sqrt( c( thresh.ukf$P.pred[-K] ) ), col='red', lwd=2, lty=2 )
 # lines( 1:K, thresh.ukf$m - sqrt( c( thresh.ukf$P.pred[-K] ) ), col='red', lwd=2, lty=2 )
 lines( 1:K, v.x, lwd=2 )
-legend( 'bottomright', c( 'x', 'Threshold filter', 'Unscented Kalman Filter', 
-                      'Monte Carlo GF', 'Gaus-Hermite Filter', 'Signal' ), 
+legend( 'bottomright', c( 'x', 'Threshold filter', 'Exact Gaussian Filter',
+                          'Unscented Kalman Filter', 'Signal' ), 
         lwd=c(2,2,2,1,1,0), lty=c(1,1,1,2,3, NA), pch=c(NA,NA,NA,NA,NA,19), bty='n',
         col=c( 'black','blue', 'red', 'red', 'red', alpha( 'darkgreen', .5) ))
 abline( h=0, lwd=.5 )
 
-# plot( 1:K, sqrt(thresh.ukf$P.pred[-(K+1)]), type='l', lwd=2, col='red' )
-# lines( 1:K, sqrt(thresh[-(K+1),2]), type='l', lwd=2, col='blue' )
+plot( 1:K, sqrt(thresh[-(K+1),2]), type='l', lwd=2, col='red' )
+lines( 1:K, sqrt(thresh[-(K+1),2]), type='l', lwd=2, col='blue' )
 
 rmse <- sqrt( cumsum( ( v.x - thresh[-(K+1),1] ) ^ 2 ) / 1:K )
+rmse.gf <- sqrt( cumsum( ( v.x - thresh.gf[-(K+1),1] ) ^ 2 ) / 1:K )
 rmse.ukf <- sqrt( cumsum( ( v.x - thresh.ukf$m.pred[-(K+1)] ) ^ 2 ) / 1:K )
-plot( c(1,K), range( rmse, rmse.ukf ), type='n', xlab='Period', ylab='Rolling RMSE' )
+plot( c(1,K), range( rmse, rmse.gf ), type='n', xlab='Period', ylab='Rolling RMSE' )
 lines( 1:K, rmse, col='blue', lwd=2)
-lines( 1:K, rmse.ukf, col='red', lwd=2 )
+lines( 1:K, rmse.gf, col='red', lwd=2 )
 
 bias <- cumsum( ( v.x - thresh[-(K+1),1] ) ) / 1:K
+bias.gf <- cumsum( ( v.x - thresh.gf[-(K+1),1] ) ) / 1:K
 bias.ukf <- cumsum( v.x - thresh.ukf$m.pred[-(K+1)] ) / 1:K 
-plot( c(1,K), range( bias, bias.ukf ), type='n', xlab='Period', ylab='Rolling bias' )
+plot( c(1,K), range( bias, bias.gf ), type='n', xlab='Period', ylab='Rolling bias' )
 lines( 1:K, bias, col='blue', lwd=2)
-lines( 1:K, bias.ukf, col='red', lwd=2 )
+lines( 1:K, bias.gf, col='red', lwd=2 )
 abline( h=0, lwd=.5 )
 
 #### Now generate a bunch of simulations and see the properties of the errors ###
 set.seed(4321)
 n.sim <- 10000
-n.pds <- 10
+n.pds <- 20
 multi.x <- multi_ar1_sim( n.sim, n.pds, rho, 0, sig.eps )
 multi.theta.hat <- 0.0 * multi.x # multi_norm_thresh( n.sim, n.pds, rho, sig.eps )
 multi.y <- 1 * ( multi.x > multi.theta.hat )
 multi.thresh <- multi_thresh_filter( multi.x, multi.theta.hat, multi.y, 
                                      c( 0, sig.eps^2 ), sig.eps, rho )
+multi.gauss <- multi_gauss_filter( multi.x, multi.theta.hat, multi.y, 
+                                     c( 0, sig.eps^2 ), sig.eps, rho )
 err <- multi.thresh$mu[,-(n.pds+1)] - t( multi.x )
 bias <- apply( err, 2, mean )
 rmse <- apply( err, 2, sd )
-# sig.mean <- apply( sqrt( multi.thresh$sig2 ), 2, mean )
+mse <- apply( err, 2, var )
+err.gf <- multi.gauss$mu[,-(n.pds+1)] - t( multi.x )
+bias.gf <- apply( err.gf, 2, mean )
+rmse.gf <- apply( err.gf, 2, sd )
+mse.gf <- apply( err.gf, 2, var )
+
+sig.mean <- apply( sqrt( multi.thresh$sig2 ), 2, mean )
 multi.thresh.ukf <- list( m.pred=0*multi.thresh$mu, P.pred=0*multi.thresh$sig2 )
 for( i in 1:n.sim ){
   temp <- ukf.compute( 0, sig.eps^2, multi.y[,i], f, g, Q, R, 1, 
@@ -116,11 +128,14 @@ for( i in 1:n.sim ){
 err.ukf <- multi.thresh.ukf$m.pred[,-(n.pds+1)] - t( multi.x )
 bias.ukf <- apply( err.ukf, 2, mean )
 rmse.ukf <- apply( err.ukf, 2, sd )
-
+mse.ukf <- apply( err.gf, 2, var )
 
 #### THIS CHART INCLUDED ####
-plot( 1:n.pds, rmse.ukf, col='red', lwd=2, type='l', xlab='Periods', ylab='RMSE' )
+plot( 1:n.pds, rmse.ukf, col='red', lty=2, lwd=2, type='l', xlab='Periods', ylab='RMSE' )
+lines( 1:n.pds, rmse.gf, col='red', lwd=2 )
 lines( 1:n.pds, rmse, col='blue', lwd=2 )
+# lines( 1:20, sqrt(apply(multi.gauss$sig2[,-(n.pds+1)],2,mean)), lty=2, col='red' )
+
 
 #### NOW DO CONDITIONAL BIAS CHARTS ####
 n.pds <- 100000
@@ -132,10 +147,12 @@ y.lr <- 1 * ( x.lr > theta.hat.lr )
 
 # Create the filters
 thresh.lr <- thresh_filter( c(0,sig.eps^2), theta.hat.lr, sig.eps, rho, y.lr )
+thresh.lr.gf <- gauss_filter( c(0,sig.eps^2), theta.hat.lr, sig.eps, rho, y.lr )
 thresh.lr.ukf <- ukf.compute( 0, sig.eps^2, y.lr, f, g, Q, R, 1, alpha=1, kappa=kappa )
 
 # De-burn
 thresh.lr <- thresh.lr[-(1:burn),]
+thresh.lr.gf <- thresh.lr.gf[-(1:burn),]
 m.thresh.lr.ukf <- cbind( thresh.lr.ukf$m.pred[-(1:burn)], 
                           thresh.lr.ukf$P.pred[-(1:burn)] )
 x.lr <- x.lr[-(1:burn)]
@@ -143,26 +160,36 @@ y.lr <- y.lr[-(1:burn)]
 
 # Create the conditional biases
 bias.pos.lr <- mean( thresh.lr[-(n.pds+1),1][y.lr==1] - x.lr[y.lr==1] )
+bias.pos.lr.gf <- mean( thresh.gf[-(n.pds+1),1][y.lr==1] - x.lr[y.lr==1] )
 bias.pos.lr.ukf <- mean( m.thresh.lr.ukf[-(n.pds+1),1][y.lr==1] - x.lr[y.lr==1] )
 bias.neg.lr <- mean( thresh.lr[-(n.pds+1),1][y.lr==0] - x.lr[y.lr==0] )
+bias.neg.lr.gf <- mean( thresh.lr.gf[-(n.pds+1),1][y.lr==0] - x.lr[y.lr==0] )
 bias.neg.lr.ukf <- mean( m.thresh.lr.ukf[-(n.pds+1),1][y.lr==0] - x.lr[y.lr==0] )
 n.same <- sequence(rle(y.lr)$lengths)
     # The number of identical signals
 table( n.same, y.lr )
 bias.p.seq <- c( by( thresh.lr[-(n.pds+1),1][y.lr==1] - x.lr[y.lr==1], 
                   n.same[y.lr==1], mean ) )[-1]
+bias.p.seq.gf <- c( by( thresh.lr.gf[-(n.pds+1),1][y.lr==1] - x.lr[y.lr==1], 
+                     n.same[y.lr==1], mean ) )[-1]
 bias.p.seq.ukf <- c( by( m.thresh.lr.ukf[-(n.pds+1),1][y.lr==1] - x.lr[y.lr==1], 
                   n.same[y.lr==1], mean ) )[-1]
 bias.n.seq <- c( by( thresh.lr[-(n.pds+1),1][y.lr==0] - x.lr[y.lr==0], 
                   n.same[y.lr==0], mean ) )[-1]
+bias.n.seq.gf <- c( by( thresh.lr.gf[-(n.pds+1),1][y.lr==0] - x.lr[y.lr==0], 
+                     n.same[y.lr==0], mean ) )[-1]
 bias.n.seq.ukf <- c( by( m.thresh.lr.ukf[-(n.pds+1),1][y.lr==0] - x.lr[y.lr==0], 
                   n.same[y.lr==0], mean ) )[-1]
 
 rmse.p.seq <- c( by( thresh.lr[-(n.pds+1),1][y.lr==1] - x.lr[y.lr==1], 
                      n.same[y.lr==1], function(x) sqrt(mean(x^2)) ) )[-1]
+rmse.p.seq.gf <- c( by( thresh.lr.gf[-(n.pds+1),1][y.lr==1] - x.lr[y.lr==1], 
+                     n.same[y.lr==1], function(x) sqrt(mean(x^2)) ) )[-1]
 rmse.p.seq.ukf <- c( by( m.thresh.lr.ukf[-(n.pds+1),1][y.lr==1] - x.lr[y.lr==1], 
                          n.same[y.lr==1], function(x) sqrt(mean(x^2)) ) )[-1]
 rmse.n.seq <- c( by( thresh.lr[-(n.pds+1),1][y.lr==0] - x.lr[y.lr==0], 
+                     n.same[y.lr==0], function(x) sqrt(mean(x^2)) ) )[-1]
+rmse.n.seq.gf <- c( by( thresh.lr.gf[-(n.pds+1),1][y.lr==0] - x.lr[y.lr==0], 
                      n.same[y.lr==0], function(x) sqrt(mean(x^2)) ) )[-1]
 rmse.n.seq.ukf <- c( by( m.thresh.lr.ukf[-(n.pds+1),1][y.lr==0] - x.lr[y.lr==0], 
                          n.same[y.lr==0], function(x) sqrt(mean(x^2)) ) )[-1]
@@ -173,8 +200,10 @@ plot( c(1,10), range( bias.p.seq[1:10], bias.p.seq.ukf[1:10],
                       bias.n.seq[1:10], bias.n.seq.ukf[1:10] ), type='n' )
 lines( 1:10, bias.p.seq[1:10], lwd=2, col='blue' )
 lines( 1:10, bias.n.seq[1:10], lwd=2, col='blue', lty=2 )
-lines( 1:10, bias.p.seq.ukf[1:10], lwd=2, col='red' )
-lines( 1:10, bias.n.seq.ukf[1:10], lwd=2, col='red', lty=2 )
+lines( 1:10, bias.p.seq.gf[1:10], lwd=2, col='red' )
+lines( 1:10, bias.n.seq.gf[1:10], lwd=2, col='red', lty=2 )
+lines( 1:10, bias.p.seq.ukf[1:10], col='red' )
+lines( 1:10, bias.n.seq.ukf[1:10], col='red', lty=2 )
 abline(h=0, lwd=.5)
 
 plot( c(1,10), range( 0, rmse.p.seq[1:10], rmse.p.seq.ukf[1:10], 
--- momErr.cpp
+++ momErr.cpp
@@ -92,7 +92,7 @@ arma::rowvec mu_sig2_update( arma::rowvec mu_sig2, double theta_hat,
                           - 74 / pow( psi, 7 ) + 706 / pow( psi, 9 ) ) ;
   }
   double mu_update = rho * ( mu + sign * sig * haz_raz) ;
-  double sig2_update = pow( sig_eps, 2 ) + pow( rho, 2 ) * 
+  double sig2_update = pow( sig_eps, 2 ) + pow( rho, 2 ) * sig2 *
                         ( 1 - pow( haz_raz, 2 ) + sign * psi * haz_raz ) ;
       // The updated distribution parameters
   rowvec out(2) ;
@@ -115,6 +115,61 @@ arma::mat thresh_filter( arma::rowvec mu_sig2_0, arma::rowvec theta_hat,
   }
   return out ;
 }
+
+// [[Rcpp::export]]
+arma::rowvec mu_sig2_update_gf( arma::rowvec mu_sig2, double theta_hat,
+                              double sig_eps, double rho, int y ){
+// Computes the updated mu, sigma^2 pair for the Gaussian filter
+  double mu = mu_sig2(0) ;
+  double sig2 = mu_sig2(1) ;
+  double sig = sqrt( sig2 ) ;
+  double psi = ( theta_hat - mu ) / sig ;
+      // Basics
+  boost::math::normal norm_dist( 0, 1 ) ;
+      // Instantiate a standard normal
+  double num = pdf( norm_dist, psi ) ;
+      // The numerator
+  double denom_h = 1 - cdf( norm_dist, psi ) ;
+  double denom_r = cdf( norm_dist, psi ) ;
+      // The denominators
+  double haz = num / denom_h ;
+  double raz = num / denom_r ;
+      // The (reverse) hazard rate
+  double sign = ( y == 1 ) ? 1 : -1 ;
+  if( denom_h < 1e-13 ){
+    haz = psi + 1 / psi - 2 / pow( psi, 3 ) + 10 / pow( psi, 5 ) 
+                          - 74 / pow( psi, 7 ) + 706 / pow( psi, 9 ) ;
+  }     // Numerical correction for the hazard rate
+  if( denom_r < 1e-13 ){
+    raz = - ( psi + 1 / psi - 2 / pow( psi, 3 ) + 10 / pow( psi, 5 ) 
+                          - 74 / pow( psi, 7 ) + 706 / pow( psi, 9 ) ) ;
+  }     // Numerical correction for the reverse hazard rate
+  double mu_update = rho * ( mu + sig * ( ( y == 1 ) ? haz : - raz ) ) ;
+  double sig2_update = pow( sig_eps, 2 ) + pow( rho, 2 ) * sig2 * 
+                              ( 1 - haz * raz ) ;
+      // The updated distribution parameters
+  rowvec out(2) ;
+  out << mu_update << sig2_update ;
+      // Format the output
+  return out ;
+}
+
+
+// [[Rcpp::export]]
+arma::mat gauss_filter( arma::rowvec mu_sig2_0, arma::rowvec theta_hat, 
+                            double sig_eps, double rho, arma::irowvec y ){
+// Apply the threshold filter
+  int K = y.n_elem ;
+      // Number of periods
+  mat out = zeros( K + 1, 2 ) ;
+  out.row(0) = mu_sig2_0 ;
+      // Initialize the output matrix
+  for( int i=0; i < K ; i++ ){
+    out.row(i+1) = mu_sig2_update_gf( out.row(i), theta_hat(i), sig_eps, rho, y(i) ) ;
+  }
+  return out ;
+}
+
 
 // [[Rcpp::export]]
 arma::mat multi_ar1_sim( int n_sim, int n_pds, double rho, double x0, 
@@ -174,6 +229,37 @@ List multi_thresh_filter( arma::mat x, arma::mat theta_hat, arma::imat y,
   return out ;
 }
 
+// [[Rcpp::export]]
+List multi_gauss_filter( arma::mat x, arma::mat theta_hat, arma::imat y, 
+                          arma::rowvec mu_sig2_0, double sig_eps, double rho ){
+// Returns a list of the mean and variances from applying the threshold filter
+// to a large sample of simulations
+  int n_pds = x.n_rows ;
+  int n_sim = x.n_cols ;
+      // The parameters of the simulation(s)
+  mat temp = zeros( n_sim, 2 ) ;
+      // Temporary container for the processed signal
+  mat mu = zeros( n_pds + 1, n_sim ) ;
+  mat sig2 = zeros( n_pds + 1, n_sim ) ;
+      // Initialize the output containers
+  mat theta_hat_T = trans(theta_hat) ;
+  imat y_T = trans(y) ;
+      // Transpose y
+  for( int i=0 ; i < n_sim ; i++ ){
+    temp = gauss_filter( mu_sig2_0, theta_hat_T.row(i), sig_eps, rho, y_T.row(i) ) ;
+//        Rcout << "temp:\n" << temp << std::endl ;
+    mu.col(i) = temp.col(0) ;
+    sig2.col(i) = temp.col(1) ;
+  }
+  mat mu_t = trans(mu) ;
+  mat sig2_t = trans(sig2) ;
+      // Format the output matrices
+  List out ;
+  out["mu"] = mu_t ;
+  out["sig2"] = sig2_t ;
+      // The output list
+  return out ;
+}
 
 
 // To add:
